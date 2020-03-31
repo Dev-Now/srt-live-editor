25b87f7959c74b6cfd1b4911f469369f5db1c3a2
Ghazi Majdoub <majdoub.ghazi@gmail.com>
2020-03-31 13:43:22 -0400

    initial commit

---
+++ TarSRTEditorEditPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TarjamatSRTEditor
+{
+    /// <summary>
+    /// Interaction logic for TarSRTEditorEditPage.xaml
+    /// </summary>
+    public partial class TarSRTEditorEditPage : Page
+    {
+        public TarSRTEditorEditPage(string szVidPath, string szSrtPath, string szOutpPath)
+        {
+            InitializeComponent();
+
+            // configure VideoEl and Subtitles text box
+            if (File.Exists(szVidPath))
+            {
+                VideoEl.Source = new Uri(szVidPath);
+                PlayCtrl.Content = "Play";
+                VideoEl.Stop();
+                // timer
+                DispatcherTimer timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromMilliseconds(10);
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+            else
+            {
+                PlayCtrl.IsEnabled = false;
+                ResetCtrl.IsEnabled = false;
+                VideoPositionLbl.Content = "00:00.000";
+                VideoPositionSlider.IsEnabled = false;
+                VideoSpanLbl.Content = "00:00.000";
+            }
+        }
+
+        private void Init_Video_Position_Ctrls(object sender, RoutedEventArgs e)
+        {
+            VideoPositionLbl.Content = "00:00.000";
+            VideoSpanLbl.Content = VideoEl.NaturalDuration.TimeSpan.ToString(@"mm\:ss\.fff");
+            VideoPositionSlider.Minimum = 0;
+            VideoPositionSlider.Maximum = (int)VideoEl.NaturalDuration.TimeSpan.TotalMilliseconds;
+            VideoPositionSlider.Value = 0;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (VideoEl.Source != null)
+            {
+                if (VideoEl.NaturalDuration.HasTimeSpan)
+                {
+                    VideoPositionLbl.Content = VideoEl.Position.ToString(@"mm\:ss\.fff");
+                    VideoPositionSlider.Value = (int)VideoEl.Position.TotalMilliseconds;
+                }
+            }
+        }
+
+        private void Play_Pause_Video(object sender, RoutedEventArgs e)
+        {
+            if (PlayCtrl.Content.ToString() == "Play")
+            {
+                VideoEl.Play();
+                PlayCtrl.Content = "Pause";
+            }
+            else
+            {
+                VideoEl.Pause();
+                PlayCtrl.Content = "Play";
+            }
+        }
+
+        private void Reset_Video(object sender, RoutedEventArgs e)
+        {
+            VideoEl.Stop();
+            PlayCtrl.Content = "Play";
+        }
+    }
+}
---
+++ TarSRTEditorHome.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TarjamatSRTEditor
+{
+    /// <summary>
+    /// Interaction logic for TarSRTEditorHome.xaml
+    /// </summary>
+    public partial class TarSRTEditorHome : Page
+    {
+        public TarSRTEditorHome()
+        {
+            InitializeComponent();
+        }
+
+        private string szVidPath;
+        private string szSrtPath;
+        private string szOutpPath;
+
+        //public string szVideoFile { get { return szVidPath; } }
+        //public string szSrtFile { get { return szSrtPath; } }
+        //public string szOutpFile { get { return szOutpPath; } }
+
+        private void Browse_Dialog(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+            string senderName = ((Button)sender).Name;
+            ofd.Filter = (senderName == "VideoFileBrowseBtn")?
+                "Video files (*.mp4)|*.mp4|Video files (*.wmv)|*.wmv|Video files (*.wav)|*.wav|Video files (*.ogg)|*.ogg":
+                "Subtitles files (*.srt)|*.srt";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if(senderName == "VideoFileBrowseBtn")
+                {
+                    VideoFileTxBx.Text = ofd.FileName;
+                }
+                else
+                {
+                    SrtFileTxBx.Text = ofd.FileName;
+                    OutputFileTxBx.Text = ofd.FileName.Replace(".srt", "-modified.srt");
+                }
+            }
+        }
+
+        private void Open_Editor_Page(object sender, RoutedEventArgs e)
+        {
+            // save the new files paths
+            szVidPath = VideoFileTxBx.Text;
+            szSrtPath = SrtFileTxBx.Text;
+            szOutpPath = OutputFileTxBx.Text;
+            // navigate to Editor Page
+            TarSRTEditorEditPage EditPage = new TarSRTEditorEditPage(szVidPath, szSrtPath, szOutpPath);
+            this.NavigationService.Navigate(EditPage);
+        }
+    }
+}
