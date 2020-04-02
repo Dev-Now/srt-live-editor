bdcf7fdf9d4e1c87d2f508feb325ef313539758a
Ghazi Majdoub <majdoub.ghazi@gmail.com>
2020-04-02 09:44:34 -0400

    working on migration to mvvm pattern

--- TarSRTEditorEditPage.xaml.cs
+++ TarSRTEditorEditPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VideoControlsViewModel;
 
 namespace TarjamatSRTEditor
 {
@@ -22,71 +23,95 @@ namespace TarjamatSRTEditor
     /// </summary>
     public partial class TarSRTEditorEditPage : Page
     {
+        VideoCtrlsVM vcvm = new VideoCtrlsVM();
         public TarSRTEditorEditPage(string szVidPath, string szSrtPath, string szOutpPath)
         {
             InitializeComponent();
 
+            VideoEl.Source = new Uri(szVidPath);
+
+            DataContext = vcvm;
+            vcvm.SetSource          += (sender, e) => { VideoEl.Source = e.Source; };
+            vcvm.StopRequest        += (sender, e) => { VideoEl.Stop(); };
+            vcvm.PlayRequest        += (sender, e) => { VideoEl.Play(); };
+            vcvm.PauseRequest       += (sender, e) => { VideoEl.Pause(); };
+            vcvm.SetPosition        += (sender, e) => { VideoEl.Position = e.Position; };
+            vcvm.GetSource          += (sender, e) => { e.Source = VideoEl.Source; };
+            vcvm.HasNaturalDuration += (sender, e) => { e.HasDuration = VideoEl.NaturalDuration.HasTimeSpan; };
+            vcvm.GetDuration        += (sender, e) => { e.Duration = VideoEl.NaturalDuration.TimeSpan; };
+            vcvm.GetPosition        += (sender, e) => { e.Position = VideoEl.Position; };
+
             // configure VideoEl and Subtitles text box
-            if (File.Exists(szVidPath))
-            {
-                VideoEl.Source = new Uri(szVidPath);
-                PlayCtrl.Content = "Play";
-                VideoEl.Stop();
-                // timer
-                DispatcherTimer timer = new DispatcherTimer();
-                timer.Interval = TimeSpan.FromMilliseconds(10);
-                timer.Tick += Timer_Tick;
-                timer.Start();
-            }
-            else
-            {
-                PlayCtrl.IsEnabled = false;
-                ResetCtrl.IsEnabled = false;
-                VideoPositionLbl.Content = "00:00.000";
-                VideoPositionSlider.IsEnabled = false;
-                VideoSpanLbl.Content = "00:00.000";
-            }
+            //if (File.Exists(szVidPath))
+            //{
+            //    VideoEl.Source = new Uri(szVidPath);
+            //    PlayCtrl.Content = "Play";
+            //    VideoEl.Stop();
+            //    // timer
+            //    DispatcherTimer timer = new DispatcherTimer();
+            //    timer.Interval = TimeSpan.FromMilliseconds(10);
+            //    timer.Tick += Timer_Tick;
+            //    timer.Start();
+            //}
+            //else
+            //{
+            //    PlayCtrl.IsEnabled = false;
+            //    ResetCtrl.IsEnabled = false;
+            //    VideoPositionLbl.Content = "00:00.000";
+            //    VideoPositionSlider.IsEnabled = false;
+            //    VideoSpanLbl.Content = "00:00.000";
+            //}
         }
 
-        private void Init_Video_Position_Ctrls(object sender, RoutedEventArgs e)
-        {
-            VideoPositionLbl.Content = "00:00.000";
-            VideoSpanLbl.Content = VideoEl.NaturalDuration.TimeSpan.ToString(@"mm\:ss\.fff");
-            VideoPositionSlider.Minimum = 0;
-            VideoPositionSlider.Maximum = (int)VideoEl.NaturalDuration.TimeSpan.TotalMilliseconds;
-            VideoPositionSlider.Value = 0;
-        }
+        //private void Init_Video_Position_Ctrls(object sender, RoutedEventArgs e)
+        //{
+        //    VideoPositionLbl.Content = "00:00.000";
+        //    VideoSpanLbl.Content = VideoEl.NaturalDuration.TimeSpan.ToString(@"mm\:ss\.fff");
+        //    VideoPositionSlider.Minimum = 0;
+        //    VideoPositionSlider.Maximum = (int)VideoEl.NaturalDuration.TimeSpan.TotalMilliseconds;
+        //    VideoPositionSlider.Value = 0;
+        //}
 
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            if (VideoEl.Source != null)
-            {
-                if (VideoEl.NaturalDuration.HasTimeSpan)
-                {
-                    VideoPositionLbl.Content = VideoEl.Position.ToString(@"mm\:ss\.fff");
-                    VideoPositionSlider.Value = (int)VideoEl.Position.TotalMilliseconds;
-                }
-            }
-        }
+        //private void Timer_Tick(object sender, EventArgs e)
+        //{
+        //    if (VideoEl.Source != null)
+        //    {
+        //        if (VideoEl.NaturalDuration.HasTimeSpan)
+        //        {
+        //            VideoPositionLbl.Content = VideoEl.Position.ToString(@"mm\:ss\.fff");
+        //            VideoPositionSlider.Value = (int)VideoEl.Position.TotalMilliseconds;
+        //        }
+        //    }
+        //}
 
-        private void Play_Pause_Video(object sender, RoutedEventArgs e)
-        {
-            if (PlayCtrl.Content.ToString() == "Play")
-            {
-                VideoEl.Play();
-                PlayCtrl.Content = "Pause";
-            }
-            else
-            {
-                VideoEl.Pause();
-                PlayCtrl.Content = "Play";
-            }
-        }
+        //private void Play_Pause_Video(object sender, RoutedEventArgs e)
+        //{
+        //    if (PlayCtrl.Content.ToString() == "Play")
+        //    {
+        //        VideoEl.Play();
+        //        PlayCtrl.Content = "Pause";
+        //    }
+        //    else
+        //    {
+        //        VideoEl.Pause();
+        //        PlayCtrl.Content = "Play";
+        //    }
+        //}
 
-        private void Reset_Video(object sender, RoutedEventArgs e)
-        {
-            VideoEl.Stop();
-            PlayCtrl.Content = "Play";
-        }
+        //private void Reset_Video(object sender, RoutedEventArgs e)
+        //{
+        //    VideoEl.Stop();
+        //    PlayCtrl.Content = "Play";
+        //}
+
+        //private void Set_Video_Position(object sender, RoutedEventArgs e)
+        //{
+        //    Slider sldr = (Slider)sender;
+        //    if (sldr.IsFocused || sldr.IsMouseOver)
+        //    {
+        //        TimeSpan tsNewPos = new TimeSpan(0,0,0,0,(int)((Slider)sender).Value);
+        //        VideoEl.Position = tsNewPos;
+        //    }
+        //}
     }
 }
--- VideoCtrlsVM.cs
+++ VideoCtrlsVM.cs
@@ -10,34 +10,50 @@ using System.Windows.Input;
 
 namespace VideoControlsViewModel
 {
+    class DataReqEventArgs : EventArgs
+    {
+        public Uri Source;
+        public bool HasDuration { get; set; }
+        public TimeSpan Duration { get; set; }
+        public TimeSpan Position { get; set; }
+    }
+
+    // This is the View Model Class
     class VideoCtrlsVM
     {
-        private MediaElement vidElt;
-
-        public VideoCtrlsVM(MediaElement _vidElt)
-        {
-            vidElt = _vidElt;
-        }
-
-        //private void Init_Video_Position_Ctrls(object sender, RoutedEventArgs e)
-        //{
-        //    //VideoPositionLblContent;
-        //    //VideoSpanLblContent;
-        //    //VideoPositionSldrMax;
-        //    VideoPositionSldrValue = 0.0;
-        //}
-
+        #region Control Requests
+        public event EventHandler<DataReqEventArgs> SetSource;
+        public event EventHandler<DataReqEventArgs> StopRequest;
+        public event EventHandler<DataReqEventArgs> PlayRequest;
+        public event EventHandler<DataReqEventArgs> PauseRequest;
+        public event EventHandler<DataReqEventArgs> SetPosition;
+        #endregion
+        
+        #region Data Requests
+        public event EventHandler<DataReqEventArgs> GetSource;
+        public event EventHandler<DataReqEventArgs> HasNaturalDuration;
+        public event EventHandler<DataReqEventArgs> GetDuration;
+        public event EventHandler<DataReqEventArgs> GetPosition;
+        #endregion
+        
         #region Video Source
         public string VideoSource
         {
-            get { return (vidElt.Source!=null)? vidElt.Source.ToString() : "default-file.wav"; }
+            get 
+            {
+                DataReqEventArgs eData = new DataReqEventArgs();
+                GetSource?.Invoke(this, eData);
+                return (eData.Source!=null)? eData.Source.ToString() : "invalid-file.wav"; 
+            }
             set 
-            { 
-                vidElt.Source = File.Exists(VideoSource) ? new Uri(VideoSource) : null;
-                if (vidElt.Source!=null)
+            {
+                DataReqEventArgs eData = new DataReqEventArgs();
+                eData.Source = File.Exists(VideoSource) ? new Uri(VideoSource) : null;
+                SetSource?.Invoke(this, eData);
+                if (eData.Source!=null)
                 {
                     PlayCtrlContent = "Play";
-                    vidElt.Stop();
+                    StopRequest?.Invoke(this, eData); //vidElt.Stop();
                     // timer
                     DispatcherTimer timer = new DispatcherTimer();
                     timer.Interval = TimeSpan.FromMilliseconds(10);
@@ -49,12 +65,16 @@ namespace VideoControlsViewModel
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (vidElt.Source != null)
+            DataReqEventArgs eData = new DataReqEventArgs();
+            GetSource?.Invoke(this, eData);
+            if (eData.Source != null)
             {
-                if (vidElt.NaturalDuration.HasTimeSpan)
+                HasNaturalDuration?.Invoke(this, eData);
+                if (eData.HasDuration)
                 {
+                    GetPosition?.Invoke(this, eData);
                     //string dummy = VideoPositionLblContent;
-                    VideoPositionSldrValue = vidElt.Position.TotalMilliseconds;
+                    VideoPositionSldrValue = eData.Position.TotalMilliseconds;
                     //TODO: notify page to refresh properties
                 }
             }
@@ -62,54 +82,72 @@ namespace VideoControlsViewModel
         #endregion
 
         #region Play Control
-        private bool IsPlaying { get; set; }
-
+        private bool isPlaying = false;
         public bool PlayCtrlEnabled
         {
-            get { return vidElt.Source != null; }
+            get 
+            {
+                DataReqEventArgs eData = new DataReqEventArgs();
+                GetSource?.Invoke(this, eData);
+                return eData.Source != null;
+            }
         }
 
         public string PlayCtrlContent
         {
-            get { return IsPlaying ? "Pause" : "Play"; }
-            set { IsPlaying = PlayCtrlContent == "Pause"; }
+            get { return isPlaying ? "Pause" : "Play"; }
+            set { isPlaying = PlayCtrlContent == "Pause"; }
         }
 
-        private void Play_Pause_Video()
-        {
-            if (PlayCtrlContent == "Play")
-            {
-                vidElt.Play();
-            }
-            else
-            {
-                vidElt.Pause();
-            }
-        }
+        //private void Play_Pause_Video()
+        //{
+        //    if (PlayCtrlContent == "Play")
+        //    {
+        //        PlayRequest?.Invoke(this, EventArgs.Empty); //vidElt.Play();
+        //    }
+        //    else
+        //    {
+        //        PauseRequest?.Invoke(this, EventArgs.Empty); //vidElt.Pause();
+        //    }
+        //}
         #endregion
 
         #region Reset Control
         public bool ResetCtrlEnabled
         {
-            get { return vidElt.Source != null; }
+            get 
+            {
+                DataReqEventArgs eData = new DataReqEventArgs();
+                GetSource?.Invoke(this, eData);
+                return eData.Source != null; 
+            }
         }
         #endregion
 
         #region Slider Control
         public bool VideoPositionSldrEnabled
         {
-            get { return vidElt.Source != null; }
+            get 
+            {
+                DataReqEventArgs eData = new DataReqEventArgs();
+                GetSource?.Invoke(this, eData);
+                return eData.Source != null; 
+            }
         }
 
         public double VideoPositionSldrMax
         {
             get 
             {
-                if (vidElt.Source != null)
+                DataReqEventArgs eData = new DataReqEventArgs();
+                GetSource?.Invoke(this, eData);
+                if (eData.Source != null)
                 {
-                    if (vidElt.NaturalDuration.HasTimeSpan)
+                    HasNaturalDuration?.Invoke(this, eData);
+                    if (eData.HasDuration)
                     {
-                        return vidElt.NaturalDuration.TimeSpan.TotalMilliseconds;
+                        GetDuration?.Invoke(this, eData);
+                        return eData.Duration.TotalMilliseconds;
                     }
                 }
                 return 0.0;
@@ -118,8 +156,17 @@ namespace VideoControlsViewModel
 
         public double VideoPositionSldrValue
         {
-            get { return vidElt.Position.TotalMilliseconds; }
-            set { vidElt.Position = new TimeSpan(0, 0, 0, 0, Convert.ToInt32(VideoPositionSldrValue)); }
+            get 
+            {
+                DataReqEventArgs eData = new DataReqEventArgs();
+                GetPosition?.Invoke(this, eData);
+                return eData.Position.TotalMilliseconds; 
+            }
+            set {
+                DataReqEventArgs eData = new DataReqEventArgs();
+                eData.Position = new TimeSpan(0, 0, 0, 0, Convert.ToInt32(VideoPositionSldrValue)); 
+                SetPosition?.Invoke(this, eData);
+            }
         }
         #endregion
 
@@ -130,11 +177,15 @@ namespace VideoControlsViewModel
         {
             get
             {
-                if (vidElt.Source != null)
+                DataReqEventArgs eData = new DataReqEventArgs();
+                GetSource?.Invoke(this, eData);
+                if (eData.Source != null)
                 {
-                    if (vidElt.NaturalDuration.HasTimeSpan)
+                    HasNaturalDuration?.Invoke(this, eData);
+                    if (eData.HasDuration)
                     {
-                        vidPositionLbl = vidElt.Position.ToString(@"mm\:ss\.fff");
+                        GetPosition?.Invoke(this, eData);
+                        vidPositionLbl = eData.Position.ToString(@"mm\:ss\.fff");
                         return vidPositionLbl;
                     }
                 }
@@ -150,11 +201,15 @@ namespace VideoControlsViewModel
         {
             get
             {
-                if (vidElt.Source != null)
+                DataReqEventArgs eData = new DataReqEventArgs();
+                GetSource?.Invoke(this, eData);
+                if (eData.Source != null)
                 {
-                    if (vidElt.NaturalDuration.HasTimeSpan)
+                    HasNaturalDuration?.Invoke(this, eData);
+                    if (eData.HasDuration)
                     {
-                        vidSpanLbl = vidElt.NaturalDuration.TimeSpan.ToString(@"mm\:ss\.fff");
+                        GetDuration?.Invoke(this, eData);
+                        vidSpanLbl = eData.Duration.ToString(@"mm\:ss\.fff");
                         return vidSpanLbl;
                     }
                 }
